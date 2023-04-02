0b422b014b05c67ec9adac28c2df63f220793dbe
ilhan-dahir <ahayad91@gmail.com>
2023-04-02 02:15:10 -0500

    added function to add employees

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(onReady);
 
 function onReady(){
-    console.log(' 😩')
+    $('.submit-button').on('click',addEmployee);
+}
+
+//Add employees 
+function addEmployee(){
+    event.preventDefault();
+    console.log('add employee');
+    readFirstName = $('#firstName').val();
+    readLastName = $('#lastName').val();
+    readId = $('#id').val();
+    readTitle = $('#title').val();
+    readSalary = $('#salary').val();
+
 }
